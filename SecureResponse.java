53fa2b7dcbe125a3a23dd9b8a18105cac935f3e7
BagasZakaria <bagaszakaria1004@gmail.com>
2026-04-06 13:19:55 +0700

    Menambahkan SecureRespone

---
+++ SecureResponse.java
@@ -0,0 +1,31 @@
+package src;
+import src.interfaces.*;
+public class SecureResponse<T extends MedicalRecord & Confidential> {
+    private boolean success;
+    private T data;
+    private String warningMessage;
+
+    public SecureResponse(boolean success, T data, String warningMessage) {
+        this.success = success;
+        this.data = data;
+        this.warningMessage = warningMessage;
+    }
+
+    public boolean isSuccess() {
+        return this.success;
+    }
+
+    public void getInfoData() {
+        if (this.data != null) {
+            System.out.println("=== Data Info ===");
+            System.out.println("Patient ID    : " + this.data.getPatientID());
+            System.out.println("Security Level: " + this.data.getSecurityLevel());
+        } else {
+            System.out.println("Data Info: Tidak ada data");
+        }
+    }
+
+    public String getWarningMessage() {
+        return this.warningMessage;
+    }
+}
